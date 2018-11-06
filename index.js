b38e6224d79255028ba8ad23b6aff9e3b65ea020
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 02:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ class Driver {
     return parseInt(totalYears);
   }
 }
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  avenueToInteger(avenue) {
+     return eastWest.indexOf(avenue);
+   }
+  blocksTravelled() {
+   let horizontalDistance =
+     this.avenueToInteger(this.endingLocation.horizontal) -
+     this.avenueToInteger(this.beginningLocation.horizontal);
+   let verticalDistance =
+     this.endingLocation.vertical - this.beginningLocation.vertical;
+   return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
+  }
+  estimatedTime(duringPeak) {
+    if (duringPeak) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+}
