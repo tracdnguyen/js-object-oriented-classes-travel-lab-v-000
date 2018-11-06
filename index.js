e9ccf8b909718cf00d28aafb4df97bce7f2502bf
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 02:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 class Driver {
   constructor(name, string) {
-    this.name = name
+    this.name = name;
     this.startDate = new Date(string);
+  };
+
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 0, 1);
+    let totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
+    return parseInt(totalYears);
   }
 }
