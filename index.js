84f15e1870f3ca2368ed94a229cfad340425c5f2
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-06 02:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,16 +18,18 @@ class Route {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
+  
   avenueToInteger(avenue) {
-     return eastWest.indexOf(avenue);
-   }
+    return eastWest.indexOf(avenue);
+  }
+  
   blocksTravelled() {
    let horizontalDistance =
      this.avenueToInteger(this.endingLocation.horizontal) -
      this.avenueToInteger(this.beginningLocation.horizontal);
    let verticalDistance =
      this.endingLocation.vertical - this.beginningLocation.vertical;
-   return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
+     return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
   }
   estimatedTime(duringPeak) {
     if (duringPeak) {
